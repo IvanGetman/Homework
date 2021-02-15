6455d0a56a478c8d853b653f168d137ea07e0667
IvanGetman <i.getman90@gmail.com>
2021-02-15 17:50:20 +0300

    Homework Lesson 11 (без пункта 3 на данный момент)

--- src/main/java/HomeworkLesson_11/HomeworkLesson_11.java
+++ src/main/java/HomeworkLesson_11/HomeworkLesson_11.java
@@ -19,7 +19,7 @@ public class HomeworkLesson_11 {
         changeElements(array, 1, 9);
         System.out.println(Arrays.toString(array));
         convertArrayToList(array);
-        System.out.println("test github");
+        System.out.println("test github 2");
     }
 
     public static String[] changeElements(String[] arr, int numElem1, int numElem2) {
